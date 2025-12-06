391a83f0a1f2a6c014d76694ba27a046af4cf60d
Gajendra N L <gajendra@Gajendras-MacBook-Air.local>
2025-12-06 17:21:57 +0530

    billing service initial commit

---
+++ billing-service/src/test/java/com/gcloud/p2pms/billing_service/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcloud.p2pms.billing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
